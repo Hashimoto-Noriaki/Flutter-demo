be3675580da1492dd05a7ad67a4f0dd614f2012b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-09-11 19:27:03 +0900

    画面遷移先のUIを作成

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -9,7 +9,57 @@ class _NextPageState extends State<NextPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Next Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(38.0),
+          child: Column(
+            children: [
+              // 上部に空白を追加
+              SizedBox(height: MediaQuery.of(context).size.height * 0.2), // 画面高さの20%分の余白
+
+              // 中央揃え
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 50,
+                        child: Text('ID'),
+                      ),
+                      Container(
+                        width: 20,
+                        child: Text(':'),
+                      ),
+                      Text('毎回同じIDを表示'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 50,
+                        child: Text('PW'),
+                      ),
+                      Container(
+                        width: 20,
+                        child: Text(':'),
+                      ),
+                      Text('毎回同じパスワードを表示'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
